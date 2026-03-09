2960564a317d965b573e32524f2501e8a021f3a1
debiddosyntax <debiddosyntax@gmail.com>
2026-03-09 19:31:35 +0100

    fixed

--- backend/build.sh
+++ backend/build.sh
@@ -5,19 +5,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
-
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-import os
-User = get_user_model()
-email = os.environ.get('SUPERUSER_EMAIL')
-user = User.objects.get(email=email)
-user.username = 'admin'
-user.firstname = 'Admin'
-user.lastname = 'Admin'
-user.is_staff = True
-user.is_superuser = True
-user.set_password(os.environ.get('SUPERUSER_PASSWORD'))
-user.save()
-print('Superuser updated.')
-"
