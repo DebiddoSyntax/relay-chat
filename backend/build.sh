adf9b4f949144b81304a6a7fa12b1386264541fb
debiddosyntax <debiddosyntax@gmail.com>
2026-03-09 13:56:50 +0100

    fix

--- backend/build.sh
+++ backend/build.sh
@@ -5,3 +5,12 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+import os
+User = get_user_model()
+if not User.objects.filter(email=os.environ.get('SUPERUSER_EMAIL')).exists():
+    User.objects.create_superuser(email=os.environ.get('SUPERUSER_EMAIL'), password=os.environ.get('SUPERUSER_PASSWORD'))
+print('Superuser ready.')
+"
